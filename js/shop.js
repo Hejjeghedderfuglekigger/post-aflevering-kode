a0cab9d83a07eb9104035fd1f8b74b2eda783216
Martin <martinboesen@hotmail.com>
2021-01-14 13:45:10 +0100

    Kategorier til shop

--- js/shop.js
+++ js/shop.js
@@ -17,6 +17,44 @@ $(document).ready(function () {
     $(".product_showcase").mouseout(function () {
         $(this).children(".see-more-text").css("z-index", "-10");
     });
+    
+    
+    var productPic = $('.product-and-info');
+    var picButtons = $('.category-button');
+    setButtonActive(0);
+    
+    function showProducts(n) {
+        var productToShow = $(n);
+        var i;
+        
+        for (i = 0; i < productPic.length; i++) {
+            productPic[i].style.display = "none";
+        }
+        
+        for (i = 0; i < productToShow.length; i++) {
+            productToShow[i].style.display = "block";
+        }
+    }
+    
+    function setButtonActive(n) {
+        var i;
+        
+        for (i = 0; i < picButtons.length; i++) {
+            picButtons[i].className = picButtons[i].className.replace(" active", "");
+        }
+        
+        picButtons[n].className += " active";
+    }
+    
+    $('#all').click(function(){showProducts('.product-and-info'); setButtonActive(0)});
+    $('#female').click(function(){showProducts('.female'); setButtonActive(1)});
+    $('#male').click(function(){showProducts('.male'); setButtonActive(2)});
+    $('#tops').click(function(){showProducts('.top'); setButtonActive(3)});
+    $('#t-shirts').click(function(){showProducts('.t-shirt'); setButtonActive(4)});
+    $('#blazers').click(function(){showProducts('.blazer'); setButtonActive(5)});
+    $('#kimonos').click(function(){showProducts('.kimono'); setButtonActive(5)});
+    
+    
     // ... Kode slut ...
 
 });
